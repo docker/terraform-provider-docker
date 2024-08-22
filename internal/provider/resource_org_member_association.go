54bbce5b07825966ab5ec58d4b2f8cf9edf55c6e
Ryan Hristovski <61257223+ryanhristovski@users.noreply.github.com>
2024-08-22 16:24:15 -0400

    remove unused import

--- internal/provider/resource_org_member_association.go
+++ internal/provider/resource_org_member_association.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/docker/terraform-provider-docker/internal/pkg/hubclient"
 	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
-	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"

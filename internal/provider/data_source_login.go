be7e65432b33344dceb8a85899bf4bb48da011f7
Nick Santos <nick.santos@docker.com>
2024-10-23 14:58:09 -0400

    provider: add a login data source for getting the current user
    fixes https://github.com/docker/terraform-provider-docker/issues/46
    
    Signed-off-by: Nick Santos <nick.santos@docker.com>

---
+++ internal/provider/data_source_login.go
@@ -0,0 +1,94 @@
+/*
+   Copyright 2024 Docker Terraform Provider authors
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package provider
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/docker/terraform-provider-docker/internal/hubclient"
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+var (
+	_ datasource.DataSource              = &LoginDataSource{}
+	_ datasource.DataSourceWithConfigure = &LoginDataSource{}
+)
+
+func NewLoginDataSource() datasource.DataSource {
+	return &LoginDataSource{}
+}
+
+type LoginDataSource struct {
+	client *hubclient.Client
+}
+
+type LoginDataSourceModel struct {
+	Username types.String `tfsdk:"username"`
+}
+
+func (d *LoginDataSource) Metadata(ctx context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_login"
+}
+
+func (d *LoginDataSource) Schema(ctx context.Context, req datasource.SchemaRequest, resp *datasource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		MarkdownDescription: `Reads current login info.`,
+
+		Attributes: map[string]schema.Attribute{
+			"username": schema.StringAttribute{
+				MarkdownDescription: "Current username",
+				Computed:            true,
+			},
+		},
+	}
+}
+
+func (d *LoginDataSource) Configure(ctx context.Context, req datasource.ConfigureRequest, resp *datasource.ConfigureResponse) {
+	// Prevent panic if the provider has not been configured.
+	if req.ProviderData == nil {
+		return
+	}
+
+	client, ok := req.ProviderData.(*hubclient.Client)
+
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Unexpected Data Source Configure Type",
+			fmt.Sprintf("Expected *hubclient.Client, got: %T. Please report this issue to the provider developers.", req.ProviderData),
+		)
+
+		return
+	}
+
+	d.client = client
+}
+
+func (d *LoginDataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+	var data LoginDataSourceModel
+	resp.Diagnostics.Append(req.Config.Get(ctx, &data)...)
+
+	data.Username = types.StringValue(d.client.Username())
+
+	diags := resp.State.Set(ctx, &data)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+}
